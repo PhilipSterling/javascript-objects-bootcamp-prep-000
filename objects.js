8c7f8c3459e376f2413a129f0d334e5c7bc164c1
Philip Sterling <philipssterlings@gmail.com>
2019-05-23 08:01:06 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ function updatePlaylist(playlist,artist,song){
   playlist[artist] = song
   return playlist
 }
+function updatePlaylist(playlist,artist){
+  playlist[artist] = song
+  return playlist
+}
