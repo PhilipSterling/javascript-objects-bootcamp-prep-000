be0152e1a72004cf5bb3c50eefca9e8dd503ea4b
Philip Sterling <philipssterlings@gmail.com>
2019-05-23 08:02:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
-var playlist = {theShins: "Australia"};
+var playlist = {};
 function updatePlaylist(playlist,artist,song){
   playlist[artist] = song
   return playlist
 }
-function updatePlaylist(playlist,artist){
+function removeFromPlaylist(playlist,artist){
   delete playlist[artist]
   return playlist
 }
